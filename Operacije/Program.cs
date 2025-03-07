a94bc50a220a1fc894023d8d16fc3e3ec9806617
TjasaZilavec <tjasa.zilavec@netica.si>
2025-03-07 13:50:39 +0100

    operacije naloge skoraj rešene

--- Operacije/Program.cs
+++ Operacije/Program.cs
@@ -8,40 +8,94 @@
             /* Naloga 3.1.2.1. Definirajte dve celostevilski spremenljivki slo in hr. Spremenljivka slo naj kot vrednost dobi (priblizno) stevilo prebivalcev Slovenije, spremenljivka hr pa (priblizno) vrednost prebivalcev Hrvaske. Na zaslon izpisite njun produkt in rezultat preverite se s kalkulatorjem. Ali je vas rezultat pravilen in zakaj ne? */
 
 
+            int slo = 2000000;
+            int hr = 3800000;
+            int produkt = slo * hr;
+            Console.WriteLine("produkt:" + produkt);
+            //rezultat ni pravilen verjetno ker je velika številka
+
+
             /* Naloga 3.1.2.2. V ukazno vrstico izpisite visine vseh druzinskih clanov in vsoto le-teh,
             pri cemer ne smete definirati nobene spremenljivke. */
+
+
+            Console.WriteLine(2.00 + 1.65);
 
 
             /* Naloga 3.1.2.3. Za vsakega druzinskega clana deklarirajte spremenljivko, ki shranjuje
             njegovo starost, nato na zaslon izpisite povprecno starost vase druzine na vsaj dve decimalki. */
 
 
+            int starost1 = 61;
+            int starost2 = 61;
+            int starost3 = 36;
+            int starost4 = 32;
+            double povprecje = (double)(starost1 + starost2 + starost3 + starost4) / 4;
+            Console.WriteLine("povprečna starost:" + povprecje);
+
+
             /* Naloga 3.1.2.4. Indeks telesne mase je vrednost definirana kot nasa teza v kilogramih deljena s kvadratom nase visine v metrih. Definirajte spremenljivke ustreznih tipov,
             izracunajte indeks telesne mase in ga izpisite na zaslon. */
+
+
+            double teza = 60;
+            double visina = 1.65F;
+            double index = teza / (visina * visina);
+
+            Console.WriteLine("index telsene teze: " + index);
 
 
             /* Naloga 3.1.2.5. Na zaslon izpisite dolzino poti v kilometrih, ki jo v enem prestopnem
             letu opravi vesoljska ladja, ce brez prestanka leti s hitrostjo 241 km/s. */
 
 
-            /* Naloga 3.1.2.6. Izberite si trimestno stevilo in ga shranite v spremenljivko. Nato na
-            zaslon izpiˇsite izbrano stevilo s stevkami zapisanimi v obratnem vrstnem redu. */
+            long cas = 366 * 24 * 60 * 60;
+            long pot = cas * 241;
+            Console.WriteLine($"Vesoljska ladja naredi {pot} km dolgo pot.");
+
+
+            /* Naloga 3.1.2.6. Izberite si trimestno stevilo in ga shranite v spremenljivko. Nato na zaslon izpisite izbrano stevilo s stevkami zapisanimi v obratnem vrstnem redu. */
+
+
+            int stevilo = 123;
+            int zadnjaStevka = stevilo % 10;
+            int srednjaStevka = (stevilo / 10) % 10;
+            int prvaStevka = stevilo / 100;
+
+            Console.WriteLine($"Stevilka prav obrnjena: {stevilo} in narobe obrnjena: {zadnjaStevka}{srednjaStevka}{prvaStevka}");
 
 
             /* Naloga 3.1.2.7. S pomocjo knjiznice Math in njene spremenljivke za vrednost konstante
-            π, izracunajte povrsino in volumen krogle s polmerom 4, 5 cm in ju izpisite. */
+            π, izracunajte povrsino in volumen krogle s polmerom 4,5 cm in ju izpisite. */
+
+
+            double polmer = 4.5;
+            double povrsinaKrogle = 4 * Math.PI * polmer * polmer;
+            double volumenKrogle = 4 * Math.PI * polmer * polmer * polmer / 3;
+
+            double zaokrozenaPovrsina = Math.Round(povrsinaKrogle, 3);
+            double zaokrozenVolumen = Math.Round(volumenKrogle, 3);
+
+            Console.WriteLine($"Povrsina krogle je {zaokrozenaPovrsina}, volumen krogle pa je {zaokrozenVolumen}");
 
 
             /* Naloga 3.1.2.8. Da dobimo najmanjso in najvecjo vrednost, ki jo lahko spremenljivka
             tipa short shrani, lahko uporabimo ukaz short.MinValue oziroma short.MaxValue. Na
-            zaslon izpiˇsite ti dve vrednosti skupaj z besedilom kaj predstavljata. Enako naredite tudi
-            za ushort, int, uint, long in ulong, da vidite kaksne vrednosti lahko sprejmejo.*/
+            zaslon izpisite ti dve vrednosti skupaj z besedilom kaj predstavljata. Enako naredite tudi
+            za ushort, int, uint, long in ulong, da vidite kaksne vrednosti lahko sprejmejo. */
+
+
+            Console.WriteLine($"short min: {short.MinValue} \n short max: {short.MaxValue} \n ushort min: {ushort.MinValue} \n ushort max: {ushort.MaxValue} \n int min: {int.MinValue} \n int max: {int.MaxValue} \n uint min: {uint.MinValue} \n uint max: {uint.MaxValue} \n long min: {long.MinValue} \n long max: {long.MaxValue} \n ulong min: {ulong.MinValue} \n ulong max: {ulong.MaxValue} \n");
 
 
             /* Naloga 3.1.2.9. Deklarirajte decimalno stevilo z vsaj petimi decimalkami, med katerimi
             naj bo vsaj ena manjsa od 5 in vsaj ena vecja ali enaka 5. Nato preizkusite zaokrozevanje na
             nekaj decimalnih mest. To lahko storite z uporabo ukaza Math.Round(vrednost, stevilo
             decimalk). */
+
+
+            double doubleStevilka = 1.2354726;
+            Console.WriteLine($"Zaokroženo na 6 decimalk: {Math.Round(doubleStevilka, 6)} \n Zaokroženo na 5 decimalk: {Math.Round(doubleStevilka, 5)} \n Zaokroženo na 4 decimalke: {Math.Round(doubleStevilka, 4)} \n Zaokroženo na 3 decimalke: {Math.Round(doubleStevilka, 3)} \n Zaokroženo na 2 decimalki: {Math.Round(doubleStevilka, 2)} \n Zaokroženo na 1 decimalko: {Math.Round(doubleStevilka, 1)} \n");
 
 
             /* Naloga 3.3.1.1. Deklarirajte stiri celostevilske spremenljivke a, b, c in d, ter jim dolocite
@@ -52,9 +106,26 @@
             d) b vecje ali enako d ali c manjse od a */
 
 
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            Console.WriteLine($"a:{a}, b:{b}, c:{c}, d:{d}");
+            Console.WriteLine($"a manjse ali enako b: {a <= b}");
+            Console.WriteLine($"c razlicno od d: {c != d}");
+            Console.WriteLine($"a + d vecje od c in b manjse od c: {a + d > c && b < c}");
+            Console.WriteLine($"b vecje ali enako d ali c manjse od a: {b >= d || c < a}");
+
+
             /* Naloga 3.3.1.2. Preizkusite primerjavo razlicnih znakov po velikosti. Preizkusite tako
             velike in male crke, kot tudi kaksne izmed drugih znakov. Kaj mislite, kako je doloceno
             kateri znak je vecji in kateri manjsi? */
+
+
+            char prviA = 'a';
+            char drugiA = 'A';
+
+            Console.WriteLine(prviA < drugiA);
 
 
             /* Naloga 3.3.1.3. Definirajte spremenljivki, v katero boste zapisali trenutno temperaturo
@@ -66,6 +137,8 @@
             /* Naloga 3.3.1.4. Definirajte spremenljivko, v katero boste zapisali svojo visino, in spremenljivko spol, ki bo imela vrednost true, ce ste zenska, in false, ce ste moski. S pomocjo
             primerjav dolocite vrednost spremenljivki nadpovprecna, ki bo imela vrednost true, ce je
             vasa visina vsaj 180 cm, ce ste moski, in vsaj 170 cm, ce ste zenska. */
+
+            Console.ReadLine();
         }
     }
 }
