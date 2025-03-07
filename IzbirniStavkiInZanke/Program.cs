4f44e97948c5c751622ac39fe0f83fc64e95d8ff
TjasaZil <tjasa.zilavec@gmail.com>
2025-03-07 20:09:06 +0100

    konec prvih vaj

--- IzbirniStavkiInZanke/Program.cs
+++ IzbirniStavkiInZanke/Program.cs
@@ -53,6 +53,180 @@
             /* Naloga 4.3.1.2. Z uporabo stavka break izpisite zaporedje stevil od 1 do n, tako da bo v prvi vrstici samo stevilo 1, v drugi 1 in 2 in tako dalje do zadnje vrstice, kjer bodo izpisana vsa stevila. */
 
 
+            //VAJE
+
+            /* Naloga 2.1.0.1. Napisite program, ki za dano celo stevilo preveri ali je sodo ali liho. */
+
+            int celoStevilo = 5;
+            if (celoStevilo % 2 == 0)
+            {
+                Console.WriteLine("Stevilo je sodo");
+            }
+            else
+            {
+                Console.WriteLine("Stevilo je liho");
+
+            }
+
+            /* Naloga 2.1.0.2. Napisite program, ki za dve dani realni stevili izpise vecjega izmed njiju. */
+
+            int x = 5;
+            int y = 10;
+            if (x > y)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine(y);
+
+            }
+
+            /* Naloga 2.1.0.3. Napisite program, ki izpise najvecje stevilo izmed treh danih celih stevil. */
+
+
+            /* Naloga 2.1.0.4. Napiˇsite program, ki izpise absolutno vrednost danega stevila (brez uporabe knjiznice Math). */
+
+
+            /* Naloga 2.1.0.5. Napisite program, ki za dano stevilo preveri ali je pozitivno, negativno, ali enako 0. */
+
+            x = -5;
+            if (x > 0)
+            {
+                Console.WriteLine("Pozitivno");
+            }
+            else if (x < 0)
+            {
+                Console.WriteLine("Negativno");
+
+            }
+            else
+            {
+                Console.WriteLine("Enako je 0");
+            }
+
+            /* Naloga 2.1.0.6. Napisite program, ki preveri ali je dano celo stevilo deljivo s 3, s 5, obema ali pa ni deljivo z nobenim od teh dveh stevil. Odgovor naj se izpise na zaslonu. */
+
+            x = 12;
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.WriteLine("Deljivo je s 3 in 5");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.WriteLine("Deljivo je s 5");
+            }
+            else if (x % 3 == 0)
+            {
+                Console.WriteLine("Deljivo je s 3 ");
+            }
+            else
+            {
+                Console.WriteLine("Deljivo ni z nobenim");
+            }
+
+            /* Naloga 2.1.0.7. Napisite program, ki za dana tri stevila izpise ali obstaja trikotnik s temi dolzinami stranic. Tak trikotnik obstaja, ce dolzine stranic ustrezajo tako imenovani trikotniski neenakosti, ki pravi, da je vsota dolzin poljubnih dveh stranic vedno vecja od dolzine tretje stranice. */
+
+
+            /* Naloga 2.1.0.8. Napisite program, ki za dano crko crko na zaslon izpise ali je crka soglasnik ali samoglasnik. Pri tem upostevajte tako male, kot velike crke. */
+
+
+            /* Naloga 2.1.0.9. Napisite program, ki za dano celo stevilo n med 1 in 7 izpise ime n-tega dneva v tednu. */
+
+
+            /* Naloga 2.1.0.10. Napisite program, ki za dano pozitivno celo stevilo leto izpise ali je leto prestopno ali ne. Leto je prestopno, ce je deljivo s 4, razen v primeru, ko je deljivo s 100 in ni deljivo s 400. */
+
+
+            /* Naloga 2.1.0.11. Napisite program, ki za dan datum sestavljen iz treh danih celostevilskih vrednosti (dan, mesec, leto) preveri ali je datum veljaven (to je, stevilka meseca in stevilo dni v mesecu je ustrezno) in, ce je, izpisete datum na zaslon in ali je leto prestopno. */
+
+
+            /* Naloga 2.2.0.1. Deklarirajte poljubno celostevilsko spremenljivko. Nato uporabite ternarni operator (pogojni izraz), da v spremenljivko za nize shranite vrednost “sodo”, ce je stevilo sodo in “liho”, sicer. Rezultat izpiˇsite na zaslon. */
+
+            //pogoj ? izraz1 : izraz2
+            x = 5;
+            string sodoLiho = x % 2 == 0 ? "sodo" : "liho";
+            Console.WriteLine(sodoLiho);
+
+
+            /* Naloga 2.2.0.2. Deklarirajte dve celostevilski spremenljivki in uporabite ternarni operator, da v novi spremenljivki z imenom min in max shranite manjse, oziroma vecje izmed njiju. Oba rezultata izpisite na zaslon. */
+
+            x = 10;
+            y = -3;
+            int min = x < y ? x : y;
+            int max = x < y ? y : x;
+
+            Console.WriteLine($"min: {min}, max: {max}");
+
+            /* Naloga 2.2.0.3. S pomocjo ternarnega operatorja na zaslon izpisite absolutno vrednost danega stevila. */
+
+
+            /* Naloga 2.2.0.4. Deklarirajte poljuben znak in z uporabo ternarnega operatorja shranite v novo spremenljivko za nize vrednost “crka”, ˇce je znak crka angleske abecede in prazen niz, sicer. */
+
+
+            /* Naloga 2.3.0.1. Z uporabo stavka switch za dano starost osebe preverite ali se lahko udelezi volitev ali ne. */
+
+            int starost = 17;
+
+            switch (starost)
+            {
+                case <= 17:
+                    Console.WriteLine("Ne");
+                    break;
+                default:
+                    Console.WriteLine("Da");
+                    break;
+            }
+
+            /* Naloga 2.3.0.2. Z uporabo stavka switch na zaslon izpisite ali je dano stevilo sodo ali liho. */
+
+            x = 11;
+            int ostanek = x % 2;
+            switch (x % 2 == 0)
+            {
+                case true:
+                    Console.WriteLine("Sodo");
+                    break;
+                default:
+                    Console.WriteLine("liho");
+                    break;
+            }
+
+            /* Naloga 2.3.0.3. Z uporabo stavka switch preverite ali je dana crka samoglasnik ali soglasnik. Nalogo resite tako, da uporabite le dva jedrna dela. */
+
+
+
+            /* Naloga 2.3.0.4. Z uporabo stavka switch za dano celo stevilo n med 1 in 7 izpisite ime n-tega dneva v tednu. */
+
+
+            /* Naloga 2.3.0.5. Uporabite stavek switch, da ustvarite preprost kalkulator, to je, da sestejete, odˇstejete, pomnozite, delite ali pa izracunate ostanek pri deljenju za dve poljubno izbrani celi stevili glede na moˇzne znake (+, −, ∗, /, %). */
+
+            double stevilo1 = 5.1;
+            double stevilo2 = 1.2;
+            char operacija = '*';
+
+            switch (operacija)
+            {
+                case '+':
+                    Console.WriteLine(stevilo1 + stevilo2);
+                    break;
+                case '-':
+                    Console.WriteLine(stevilo1 - stevilo2);
+                    break;
+                case '/':
+                    Console.WriteLine(stevilo1 / stevilo2);
+                    break;
+                case '*':
+                    Console.WriteLine(stevilo1 * stevilo2);
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+
+            /* Naloga 2.3.0.6. Deklarirajte spremenljivko ocena in ji kot vrednost podajte celoˇstevilsko oceno, ki jo priˇcakujete pri tem predmetu. Nato s pomoˇcjo stavka switch izpiˇsite opisno ime vaˇse priˇcakovane ocene (≤ 5 - nezadostno, 6 - zadostno, 7 - dobro, 8 in 9 - prav dobro in 10 - odliˇcno). V programu uporabite primerjalni vzorec. */
+
+            Console.ReadKey();
+
         }
     }
 }
--- SpremenljivkeInTipi/Program.cs
+++ SpremenljivkeInTipi/Program.cs
@@ -61,8 +61,8 @@
             priimek. Nato na zaslon v eni vrstici izpisite “Jaz sem IME PRIIMEK”. */
 
 
-            string ime = "Tjasa";
-            string priimek = "Zilavec";
+            string ime = "Ime";
+            string priimek = "Priimek";
             Console.WriteLine($"Jaz sem {ime} {priimek}");
 
 
@@ -71,12 +71,12 @@
             storili, ce bi lahko uporabili le eno spremenljivko? */
 
 
-            string imeInPriimek = "Tjasa Zilavec \n";
+            string imeInPriimek = "Ime Priimek \n";
             string naslov = "Ulica 123 \n";
             string posta = "1000 Ljubljana";
             Console.WriteLine($"Uporabila sem le več spremenljivk:\n {imeInPriimek} {naslov} {posta}");
 
-            string jaz = "Tjasa Zilavec \n Ulica 123 \n 1000 Ljubljana";
+            string jaz = "Ime Priimek \n Ulica 123 \n 1000 Ljubljana";
             Console.WriteLine($"Uporabila sem le eno spremenljivko:\n {jaz}");
 
 
@@ -136,6 +136,175 @@
 
 
             //VAJE
+
+            /* Naloga 1.0.0.2. Deklarirajte vsaj 6 osnovnih tipov spremenljivk in jim dolocite poljubne vrednosti. Vsako od spremenljivk tudi izpisite na zaslon. */
+
+            //tim ime = vrednost;
+
+            //logični tipi
+            bool spr1 = true; //false
+
+            //Znakovni tipi
+            char spr2 = 'a';
+
+            //celoštevilski tipi
+            int spr3 = 4; //32 bitov
+            //byte - 8 bitov
+            //short - 16 bitov
+            //long - 64 bitov
+            long spr4 = 5L;
+
+            //Decimalni tipi
+            double spr5 = 5.1; //64 bitov
+            float spr6 =4.2F;
+            decimal spr7 =6.4M;
+
+            //ni osnovni tip ampak sklicni tip
+            string spr8 = "hello";
+
+
+            /* Naloga 1.0.0.3. Programu iz prejsnje naloge dodajte nekaj enovrsticnih komentarjev. V program dodajte se en vecvrsticni komentar. */
+
+
+            /* To
+             je
+             večvrstični
+             komentar
+            */
+
+            //to je enovrstični komentar :D
+
+
+            /* Naloga 1.0.0.4. Denimo, da zelite opisati lastnosti avta: znamka, stevilo vrat, hitrost in ali je nov/rabljen. Ugotovite kateri tip podatka bi uporabili za vsako izmed lastnosti, da bi najbolje ustrezal podatku, ki ga bo spremenljivka hranila. */
+
+
+            string znamka = "Audi";
+            int steviloVrat = 5;
+            int hitrost1;
+            double hitrost2;
+            string hitrost3; //če želimo mersko enoto
+            bool jeNov1; //true ali false -> true je nov, false ni nov
+            string jeNov2; //napišeš 'rabljen' ali 'nov'
+            char jeNov3; //N->nov, R->rabljen
+
+
+
+
+            /* Naloga 1.0.0.5. Ustvarite dve spremenljivki, v eno shranite svoje ime in v drugo svoj priimek. Nato na zaslon v eni vrstici izpisite “Moje ime je: IME PRIIMEK”. */
+
+
+            string ime1 = "IME";
+            string priimek1 = "PRIIMEK";
+            Console.WriteLine($"Moje ime je: {ime1} {priimek1}");
+            //lahko tudi
+            Console.WriteLine("Moje ime je:" + ime1 + " " + priimek1);
+
+
+            /* Naloga 1.0.0.6. Na zaslon izpisite vase ime s pomocjo unicode zapisa za znake s pomocjo ukaza \u. */
+
+
+            char unicodeZnak = '\u0041';
+            Console.WriteLine(unicodeZnak);
+            string ime2 = "\u0049\u004D\u0045";
+            Console.WriteLine( ime2 );
+            char znakI = '\u0049';
+            char znakM = '\u004D';
+            char znakE = '\u0045';
+            Console.WriteLine( znakI+znakM+znakE );
+
+
+            /* Naloga 1.0.0.7. Na zaslon izpisite svoj naslov z nizi (ime in priimek, ulica in hisna stevilka, postna stevilka in posta) v treh vrsticah. */
+
+
+            string ulica = "ulica in hisna stevilka";
+            string posta1 = "posta in postna stevilka";
+            Console.WriteLine(ime + " " + priimek);
+            Console.WriteLine(ulica);
+            Console.WriteLine(posta1);
+            
+
+            /* Naloga 1.0.0.8. Program iz prejsnje naloge popravite tako, da ukaz za izpis na zaslon uporabite le enkrat. Pomagajte si z ukazom \n, ki ga morate zapisati v samem nizu. */
+
+            Console.WriteLine(ime + " " + priimek + "\n" + ulica + "\n" + posta);
+
+            /* Naloga 1.0.0.9. Ustvarite dve celostevilski spremenljivki in vanju shranite vrednosti 125 in 20. Nato na zaslon v razlicnih vrsticah izpisite njuno vsoto, razliko, produkt, deljenje in ostanek pri deljenju. Izpise primerjajte z dejanskimi vrednostmi. */
+            /* Naloga 1.0.0.10. Prejsnjo nalogo popravite tako, da pred rezultatom na zaslon v isti vrstici izpisete tudi kaj rezultat predstavlja. Nato storite enako, le da bo opis rezultata zapisan za samo vrednostjo. */
+
+            //nalogi 9 in 10
+
+            int st1 = 5;
+            int st2 = 3;
+            Console.WriteLine(st1 + st2); //seštevanje
+            Console.WriteLine(st1 - st2); //odštevanje
+            Console.WriteLine(st1 * st2); //množenje
+            Console.WriteLine(st1 / st2); //deljenje
+            Console.WriteLine(st1 % st2); //ostanek pri deljenju
+            Console.WriteLine("Vsota: " + (st1 + st2));
+            Console.WriteLine(st1 + st2 + "je vsota");
+            Console.WriteLine("Odštevanje: " + (st1 - st2));
+            Console.WriteLine(st1 - st2 + "je razlika");
+            Console.WriteLine("Množenje: " + (st1 * st2));
+            Console.WriteLine(st1 * st2 + "je produkt");
+            Console.WriteLine("Deljenje: " + (st1 / st2));
+            Console.WriteLine("Ostanek pri deljenju: " + (st1 % st2));
+
+
+            /* Naloga 1.0.0.11. Ustvarite celostevilsko spremenljivko s poljubno vrednostjo. Ustvarjeno spremenljivko spremenite tako, da jo enkrat povecate, zmanjsate, pomnozite, delite in shranite vanjo ostanek pri deljenju z 2. Uporabite krajse oblike zapisov. */
+
+            int stevilo = 5;
+            stevilo = 2;
+            stevilo += 2;
+            stevilo -= 2;
+            stevilo *= 2;
+            stevilo /= 2;
+            stevilo %= 2;
+
+
+
+            /* Naloga 1.0.0.12. Na zaslon izpisite rezultate naslednjih izrazov in jih primerjajte z dejanskimi vrednostmi.
+            3
+            a) −3 % 8
+            b) −5 + 4 ∗ 7
+            c) (25 + 3) % 8
+            d) 10 + −3 ∗ 5 / 7
+            e) 5 + 15 / 3 ∗ 2 − 8 % 3
+            */
+
+
+            /* Naloga 1.0.0.13. Za dano trimestno stevilo izpisite stevilo stotic, desetic in enic. */
+
+            /* Naloga 1.0.0.14. Ustvarite tri poljubne celostevilske spremenljivke in na zaslon izpisite njihovo povprecje. */
+
+
+            /* Naloga 1.0.0.15. Deklarirajte stiri celostevilske spremenljivke a, b, c in d, ter jim dolocite poljubno vrednost. Nato na zaslon izpisite rezultate naslednjih logicnih izrazov.
+            a) a manjse ali enako b
+            b) c razlicno od d
+            c) a + d vecje od c in b manjse od c
+            d) b vecje ali enako d ali c manjse od a
+            */
+
+            int a1 = 3;
+            int b = 5;
+            int c = 8;
+            int d = 4;
+            Console.WriteLine($"a:{a1}, b:{b}, c:{c}, d:{d}");
+            Console.WriteLine($"a manjse ali enako b: {a1 <= b}");
+            Console.WriteLine($"c razlicno od d: {c != d}");
+            Console.WriteLine($"a + d vecje od c in b manjse od c: {a1 + d > c && b < c}");
+            Console.WriteLine($"b vecje ali enako d ali c manjse od a: {b >= d || c < a1}");
+
+            /* Naloga 1.0.0.16. Deklarirajte dve spremenljivki enakega tipa, dolocite jima poljubno vrednost in ju izpisite na zaslon. Nato spremenljivkama v programu zamenjajte vrednosti in na zaslon izpisite njune nove vrednosti. */
+
+
+            /* Naloga 1.0.0.17. Izracunajte ploscino kvadrata in kroga s stranico (oziroma polmerom) enako/-im 2.5. Konstanto π dobimo s pomocjo ukaza M ath.P I. */
+
+
+            /* Naloga 1.0.0.18. Da dobimo najmanjso in najvecjo vrednost, ki jo lahko spremenljivka tipa short shrani, lahko uporabimo ukaz short.MinValue oziroma short.MaxValue. Na
+            zaslon izpisite ti dve vrednosti. Enako naredite tudi za ushort, int, uint, long in ulong, da vidite kaksne vrednosti lahko sprejmejo. */
+
+
+            /* Naloga 1.0.0.19. Deklarirajte decimalno stevilo z vsaj petimi decimalkami, med katerimi naj bo vsaj ena manjsa od 5 in vsaj ena vecja ali enaka 5. Nato preizkusite zaokrozevanje na nekaj decimalnih mest. To lahko storite z uporabo ukaza Math.Round("vrednost", stevilo decimalnih mest").*/
+
 
             Console.ReadKey();
         }
