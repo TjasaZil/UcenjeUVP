8c1cbbae27a831c61c8dc605c9e4a7143d7b656d
TjasaZil <tjasa.zilavec@gmail.com>
2025-03-10 18:19:16 +0100

    dodala naloge za datoteke in nize -> ni še rešeno

---
+++ DatotekeInNizi/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DatotekeInNizi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PREDAVANJA
+
+            /* Naloga 7.1.1.1. Zgoraj smo delali z EMSO stevilko in prebrali letnico rojstva osebe.  Pridobite se ostale podatke o osebi, ki jih lahko prepoznate iz EMSO stevilke. */
+
+            /* Naloga 7.1.1.2. Zapisite metodo, ki kot vhodna podatka dobi niz in znak, vrne pa stevilo pojavitev znaka v nizu.*/
+
+            /* Naloga 7.1.1.3. Zgornji primer s podatki o osebi smo zapisali precej naivno. V realnih aplikacijah moramo preveriti in precistiti vnose. Na primer, vrednosti za visino, tezo in starost morajo biti stevilske in iz primernih intervalov (npr. starost med 0 in (recimo) 120). Prav tako obicajno statuse izbiramo iz sifrantov, da nimamo nekaj razlicnih zapisov istega statusa ipd. Dopolnite zgornjo kodo s preverjanjem, da spremenljivke res dobijo ustrezne vrednosti in v primeru napak izpisite obvestilo, s sporocilom, kaj je slo narobe.*/
+
+            /* Naloga 7.1.1.4. Napisite metodo, ki uporabniku naroci vpis besede z dano zacetno crko in danim (slucajno izbranim) stevilom znakov med 4 in 8. Nato preverite, ce je odgovor pravilen. Nalogo lahko popestrite tako, da namesto prve crke nakljucno izberete mesto crke, ki jo mora uporabiti.*/
+
+            /* Naloga 7.1.1.5. Napisite metodo, ki za dani niz vrne stevilo razlicnih znakov v njem. */
+
+            /* Naloga 7.2.1.1. Ustvarite datoteko, ki ima enako ime kot vi in koncnico txt. Uporabite metode iz prejˇsnjih poglavij in v datoteko zapisite prvih sto prastevil, vsako v svojo vrstico.*/
+
+            /* Naloga 7.2.1.2. Nalogo 7.1.1.4 popravite tako, da bo metoda vprasanja in odgovore uporabnika zapisala tudi v datoteko.*/
+
+            /* Naloga 7.3.1.1. Nase programe pisemo v datoteke, ki so na disku shranjene v datotekah s koncnico cs. Izberite si dve in na zaslon izpisite njuno vsebino. */
+
+            /* Naloga 7.3.1.2. Na svojem disku poiscite neko tekstovno datoteko in napisite program, ki bo iz datoteke izpisal vse vrstice, ki vsebujejo enega od treh sumnikov (c, s, z). */
+
+            /* Naloga 7.3.1.3. Kodo poljubne spletne strani, ki jo berete v brskalniku, lahko tudi shranite. V brskalniku Chrome to na primer storite s pritiskom tipk CTRL + S. V kodi spletne strani povezavo (angl. hyperlink) najdemo kot vrednost atributa href, zapisano med dvema narekovajema. Primer je npr. href="https://www.example.com". Poiscite vse povezave na spletni strani, ki ste si jo shranili, in jih zapisite v novo datoteko. */
+
+            // VAJE
+
+            /* Naloga 7.0.0.1. Deklarirajte poljuben seznam nizov in ga zapisite v tekstovno datoteko katere ime izberete sami. */
+
+            /* Naloga 7.0.0.2. Vsebino datoteke iz prejsnje naloge znova preberite in jo shranite v nov seznam nizov. */
+
+            /* Naloga 7.0.0.3. Prejsnji dve nalogi spremenite v metodi. Prva naj deluje tako, da v primeru obstoja datoteke vsebino le doda v datoteko. */
+
+            /* Naloga 7.0.0.4. Napisite program, ki bo uporabniku omogocal shranjevanje podatkov o letni porabi financ. Pri tem naj ima uporabnik na voljo za vnos cene racuna in datuma placila (dan, mesec, leto). Premislite kako bi zeleli lociti posamezne stroske glede na namen porabe (hrana, gospodinjstvo, oblacila, voda, elektrika, potovanje, razno ...), kar naj bo razvidno pri zapisu v datoteko. Vsi podatki naj se ustrezno shranijo v eno datoteko. Ime datoteke za vnos naj se prebere ob delovanju programa. */
+
+            /* Naloga 7.0.0.5. Program iz prejsnje naloge ustrezno razsirite tako, se ob zagonu vsebina datoteke, katere ime poda uporabnik, najprej prebere. Nato naj ima uporabnik moznost dodajanja novih porab financ ali moznosti hitre analize stroskov. Na primer: vsota po mesecih, vsota po namenu, letna vsota, itd. */
+
+        }
+    }
+}
