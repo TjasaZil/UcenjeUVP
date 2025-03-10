50813ae84f8c0f133398e234f500d8900d658333
TjasaZil <tjasa.zilavec@gmail.com>
2025-03-10 18:05:09 +0100

    dodane metode za naloge in izjeme -> ni še rešeno

---
+++ MetodeInIzjeme/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MetodeInIzjeme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // PREDAVANJA
+
+            /* Naloga 6.1.1.1. Zapisite metodo, ki kot parameter dobi celo stevilo n, kot rezultat pa vrne seznam vseh prastevil manjsih od n. */
+
+            /* Naloga 6.1.1.2. Zapisite metodo, ki kot parameter dobi stevilo v desetiskem zapisu in ga pretvori v dvojiski zapis. Zapisite se metodo, ki zgornjo metodo izvede na vseh stevilih od 1 do n in rezultate zapise v seznam.*/
+
+            /* Naloga 6.1.1.3. Zapisite metodo, ki kot rezultat izrise sahovnico dimenzije n × m, kjer sta n in m parametra metode. */
+
+            /* Naloga 6.2.1.1. Zapisite metodo, ki uporabnika sprasuje po kvadratu danega, slucajno izbranega stevila (uporabite razred Random). Predvidite razlicne moznosti napacnih vnosov in jih ustrezno ujemite. */
+
+            // VAJE
+
+            /* Naloga 6.0.0.1. Napisite metodo, ki za dani naravni stevili a in b izracuna vrednost a in b brez uporabe knjiznice Math z eno od zank. */
+
+            /* Naloga 6.0.0.2. Napisite metodo, ki prejme celo stevilo in zanj na zaslon izpiˇse vse njegove delitelje in vrne koliko deliteljev ima. */
+
+            /* Naloga 6.0.0.3. Napisite metodo, ki prejme tabelo znakov in nek znak, ter vrne ali je podan znak v tabeli ali ne. */
+
+            /* Naloga 6.0.0.4. Napisite metodo, ki za podani naravni stevili n in m vrne tabelo vseh stevil med njima. Tabelo nato izpisite na zaslon. */
+
+            /* Naloga 6.0.0.5. Napisite metodo, ki prejme niz in znak ter vrne stevilo pojavitev znaka v nizu. */
+
+            /* Naloga 6.0.0.6. Napisite metodo, ki prejme niz in ga pretvori v tabelo znakov, ki jo nato vrne. Tabelo nato izpisite na zaslon. */
+
+            /* Naloga 6.0.0.7. Napisite metodo, ki za podan seznam besed in podano crko vrne seznam vseh besed, ki se zacnejo z dano crko. */
+
+            /* Naloga 6.0.0.8. Napisite metodo, ki vrne stevilo besed v podanem nizu. Predpostavite lahko, da so besede v nizu locene le s presledki in ne vsebujejo nobenih drugih znakov. */
+
+            /* Naloga 6.0.0.9. Napisite metodo, ki prejme seznam besed in vrne par, kjer je na prvem mestu najdaljsa beseda, na drugem pa njen indeks v seznamu. */
+
+            /* Naloga 6.0.0.10. Napisite metodo, ki vrne tabelo malih crk angleske abecede, metodo, ki vrne tabelo velikih crk angleske abecede in metodo, ki vrne tabelo stevk. */
+
+            /* Naloga 6.0.0.11. Napisite metodo, ki prejme niz in vrne trojico vrednosti, kjer je na prvem mestu stevilo crk, na drugem mestu stevilo stevk, na tretjem mestu pa stevilo preostalih znakov v nizu. */
+
+            /* Naloga 6.0.0.12. Napisite metodo, ki bo izracunala vsoto stevil v celostevilskem seznamu v primeru, da ta ni prazen. Nato v Main metodi to vsoto izpisite v kolikor je bila izracunana. */
+
+
+            /* Naloga 6.0.0.13. Napiˇsite program, ki bo od uporabnika zahteval nastavitev gesla. Geslo je ustrezno, ce ima vsaj 8 znakov, vsebuje vsaj dve crki in vsaj dve stevki, ob tem pa ima vsaj eno veliko in vsaj eno majhno crko. */
+
+            /* Naloga 6.0.0.14. Napiˇsite program, ki bo uporabniku 10-krat na zaslon izpisal nakljucno stevilo, ki ga more uporabnik vnesti. Na koncu naj se izpise stevilo pravilnih vnosov, napacnih vnosov in napak pri vnosu. */
+
+            /* Naloga 6.0.0.15. Napisite program, ki bo uporabniku omogocal vnasanje enostavnih matematicnih izrazov z dvema vrednostima v obliki enega niza, kjer je med stevilkami in operacijo le en presledek. Program naj nato vrne rezultat izraza. Uporabite metode in pa try-catch stavek za moznost deljenja z 0. */
+
+            /* Naloga 6.1.0.1. Napisite program za igranje vislic, kjer se beseda nakljucno izbere iz predhodno dolocenega seznama besed. */
+
+            /* Naloga 6.1.0.2. Napisite program za igranje stiri v vrsto v konzoli, kjer je stevilo vrstic 6 in stevilo stolpcev 7. */
+
+        }
+    }
+}
