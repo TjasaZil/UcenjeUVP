951a5afb8078ac9282bb345c5756b206f5f10d8f
TjasaZil <tjasa.zilavec@gmail.com>
2025-03-10 17:43:16 +0100

    dodani izpiti

---
+++ Izpit_2024_03_23/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Izpit_2024_03_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// NALOGA 1:
+            /// Napišite metodo RezultatiIzpita, ki ne prejme nobenega parametra,
+            /// vrača pa decimalno število. 
+            /// V njej od uporabnika zahtevajte vnos podatkov (preko ukazne vrstice) 
+            /// o študentih, ki so pisali izpit. Za vsakega študenta naj 
+            /// vpiše vpisno številko in število doseženih točk.
+            /// Podatke naj vnaša, dokler ne vnese vrednosti "X", 
+            /// ki naj pomeni izhod iz vnosa podatkov.                          [10 točk]
+            /// 
+            /// Pridobljeni podatki za vsakega študenta naj se zapišejo
+            /// kot lastnost v instanco razreda StudentData 
+            /// (ta razred bo torej imel dve lastnosti).
+            /// Vse instance shranite v seznam.                                 [5 točk]
+            /// 
+            /// V metodi v ukazno vrstico izpišite največje število doseženih točk
+            /// ter najmanjše število doseženih točk.                           [5 točk]
+            /// 
+            /// Na koncu naj metoda RezultatiIzpita vrne povprečno število doseženih točk.
+            /// Metodo še pokličite v metodi ResitevNaloge in izpišite dobljeni rezultat 
+            /// v ukazno vrstico.                                               [5 točk]
+            /// </summary>
+
+            /// <summary>
+            /// NALOGA 2:
+            /// 
+            /// Pripravite razreda Prostor in Zgradba.
+            /// Prostor naj ima kot lastnosti površino in ime prostora, Zgradba pa
+            /// naj ima za lastnost seznam prostorov.                   [3 točke]
+            /// 
+            /// Pripravite razreda StanovanjskiBlok in Hisa,
+            /// ki naj dedujeta iz razreda Zgradba.
+            /// Vsakemu od teh dveh razredov določite še po eno specifično lastnost.
+            ///                                                         [5 točk]
+            /// 
+            /// V razredu Zgradba povozite metodo ToString, da bo vračala zapis
+            /// vseh prostorov v zgradbi skupaj z njihovo površino.
+            /// Metodo ToString dopolnite v obeh podrazredih, da bo ustrezno 
+            /// izpisovala tudi njuni posebni lastnosti.                [5 točk]
+            /// 
+            /// V razredu Zgradba napišite metodo ŠteviloProstorov, 
+            /// ki bo vrnila število vseh prostorov,
+            /// katerih površina je večja od danega parametra.          [7 točk]
+            ///                                                         
+            /// V metodi ResitevNaloge ustvarite tri instance razreda Hisa 
+            /// in dve instanci razreda StanovanjskiBlok. 
+            /// Vsaki od instanc dodajte na seznam prostorov vsaj dva prostora.
+            /// Na koncu za dve od instanc izpišite število prostorov 
+            /// večjih od 10 kvadratnih metrov.                         [5 točk]
+            /// </summary>
+
+            /// <summary>
+            /// NALOGA 3:
+            /// Pripravite razreda Izpit in IzpitnaNaloga.
+            /// 
+            /// Razred IzpitnaNaloga naj vsebuje:
+            /// -   Lastnosti ZaporednaStevilka, Tezavnost (od 1 do 5), SteviloTock, Besedilo.
+            /// -   Natanko en konstruktor, ki nastavi vsaj dve lastnosti.                      
+            /// -   Povozite metodo ToString, ki naj izpiše vse podatke o nalogi.               [8 točk]
+            /// 
+            /// Razred Izpit naj vsebuje: 
+            /// -   Lastnosti ImePredmeta, ZaporednaStevilka (prvi, drugi, ...), Datum, 
+            ///     CasPisanja (tej lastnosti lahko vrednost nastavimo samo v konstruktorju!),
+            ///     Naloge (seznam nalog).
+            /// -   En konstruktor, ki omogoča določitev natanko dveh lastnosti, 
+            ///     in en prazen konstruktor, ki naj vseeno določi čas pisanja.
+            ///     V obeh konstruktorjih inicializirajte seznam nalog.
+            /// -   Povozite metodo ToString, ki naj izpiše vse podatke o izpitu.               
+            /// -   Napišite metodo, ki izpiše vse izpitne naloge, 
+            ///     katerih besedilo vsebuje vsaj 300 znakov.                                   [12 točk]
+            /// 
+            /// V metodi ResitevNaloge kreirajte instanco razreda Izpit, ki naj vsebuje vsaj pet izpitnih nalog
+            /// in izpišite podatke o vseh nalogah, pri katerih lahko dosežemo vsaj 10 točk 
+            /// in katerih težavnost je nižja od 4.                                             [5 točk]
+            /// </summary>
+
+            /// <summary>
+            /// NALOGA 4:
+            /// 
+            /// Med viri imate datoteko words_alpha.txt s seznamom angleških besed
+            /// (ena beseda v vsaki vrstici). 
+            /// V metodi imate že pripravljeno instanco razreda StreamReader,
+            /// ki odpre omenjeno datoteko.
+            /// 
+            /// Poiščite vse besede v datoteki, ki vsebujejo črko 'x' 
+            /// (veliko ali malo) in jih dodajte v seznam.                              [5 točk]
+            /// 
+            /// Med najdenimi besedami poiščite vse palindrome 
+            /// (to so besede, ki se berejo enako naprej in nazaj, npr. kisik)
+            /// in jih izpišite.                                                        [12 točk]
+            /// 
+            /// Na koncu izpišite število vseh besed, ki vsebujejo 'x' in 
+            /// ne vsebujejo 'i' ali 'a'.                                               [8 točk]
+            /// </summary>
+        }
+    }
+}
---
+++ Izpit_2024_06_18/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace Izpit_2024_06_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// NALOGA 1:
+            /// 
+            /// Napišite metodo VsoteStevk, ki kot parameter prejme seznam celih števil. 
+            /// Za vsako število iz seznama naj izračuna vsoto njegovih števk 
+            /// (npr.vsota števk števila 23 je 5) in na zaslon izpiše tista
+            /// (originalna) števila, katerih vsota je manjša od 10.                    [15 točk]
+            /// 
+            /// Metoda naj vsote števk vseh števil iz danega seznama sešteje 
+            /// in ta seštevek vrne.                                                    [5 točk]
+            /// 
+            /// V metodi ResitevNaloge pripravite seznam z vsaj petimi celimi števili, 
+            /// pokličite metodo VsoteStevk in izpišite skupno vsoto vseh števk, 
+            /// ki jo metoda vrne, v ukazno vrstico.                                    [5 točk]
+            /// </summary>
+            /// 
+
+            /// <summary>
+            /// NALOGA 2:
+            /// 
+            /// V Nemčiji ravno poteka evropsko	nogometno prvenstvo, 
+            /// mi pa želimo pripraviti razredni model za beleženje 
+            /// statistik tekem na klubskem svetovnem prvenstvu. 
+            /// V ta namen pripravite razrede Ekipa, Statistika in Tekma.
+            /// 
+            /// Razred Ekipa naj vsebuje: 
+            /// - Lastnosti Ime, Klub in Igralci.
+            /// - Natanko en konstruktor, ki nastavi lastnosti Ime in Klub.
+            /// - Metodo ToString, ki izpiše vse lastnosti ekipe.               [4 točk]
+            /// 
+            /// Razred Statistika naj vsebuje: 
+            /// - Lastnosti Goli, PosestZoge, Prekrski, RumeniKartoni in RdečiKartoni.
+            /// - Metodo ToString, ki naj izpiše vse statistične enote.         [4 točk]
+            /// 
+            /// Razred Tekma naj vsebuje: 
+            /// - Lastnosti Domači, Gosti, Datum, StatistikaDomači in StatistikaGosti. 
+            ///   Vse lastnosti naj imajo ustrezen tip.
+            /// - En konstruktor, ki omogoča zgolj določitev lastnosti Domači in Gosti.
+            /// - Metodo ToString, ki naj izpiše vse podrobnosti o tekmi.
+            /// - Metodo, ki vrne skupno število rumenih kartonov na tekmi.
+            /// - Metodo, ki vrne rezultat tekme.                               [12 točk]
+            /// 
+            /// V metodi ResitevNaloge kreirajte dve instanci razreda Ekipa, 
+            /// instanco razreda Tekma, instanci za statistiko obeh ekip na tekmi 
+            /// in na koncu na zaslon izpišite rezultat ter podatke o tekmi.    [5 točk]
+            /// </summary>
+            /// 
+
+            /// <summary>
+            /// NALOGA 3:
+            /// V podjetju ima vsak zaposleni nekaj različnih prenosnih naprav. 
+            /// Da bi imel IT oddelek nadzor nad inventarjem, pripravlja aplikacijo, 
+            /// za katero pa potrebuje vašo pomoč.
+            /// 
+            /// Napišite razred MobilnaNaprava, ki ima lastnosti LetoIzdelave, 
+            /// Cena, Ima5GModul in TipProcesorja. 
+            /// V razredu pripravite še natanko en konstruktor,  
+            /// ki kot parameter dobi leto izdelave. 
+            /// Povozite tudi metodo ToString.                                          [6 točk]
+            /// 
+            /// Za razred MobilnaNaprava pripravite tri podrazrede: 
+            /// Telefon, Tablica in Prenosnik. 
+            /// Za vsako od naprav si zamislite po dve ustrezni specifični lastnosti 
+            /// in ju implementirajte. 
+            /// V vsakem od podrazredov ustrezno popravite še metodo ToString.          [7 točk]
+            ///     
+            /// Napišite še razred Zaposleni, 
+            /// ki naj ima lastnosti Naprave (seznam naprav, ki jih ima zaposleni) 
+            /// in SkupnaVrednost (vsota cen vseh naprav zaposlenega). 
+            /// Dodajte še metodo, ki vrne najdražjo napravo zaposlenega, 
+            /// in metodo, ki vrne najstarejšo napravo zaposlenega.                     [7 točk]
+            /// 
+            /// V metodi ResitevNaloge ustvarite eno instanco razreda Zaposleni 
+            /// in na njegov seznam dodajte tri naprave različnih tipov. 
+            /// Na zaslon s pomočjo klica ustrezne metode izpišite 
+            /// ceno najdražje naprave tega zaposlenega.                                [5 točk]
+            /// </summary>
+            /// 
+
+            /// <summary>
+            /// NALOGA 4:
+            /// 
+            /// V mapi Viri imate pripravljeni dve datoteki s tabelama števil. 
+            /// Števila so v posamezni vrstici ločena s tabulatorjem.
+            /// 
+            /// Pripravite metodo VsotaSodih, v kateri za dano pot do datoteke s tabelo števil 
+            /// vsa števila po vrsti združite v en niz (brez presledkov ali tabulatorjev), 
+            /// nato v eno spremenljivko seštejte vse števke na sodih mestih v tem nizu 
+            /// in v drugo spremenljivko seštejte vse števke na lihih mestih 
+            /// (pri tem je prva števka na sodem mestu, saj je na indeksu 0). 
+            /// Metoda naj vrne razliko obeh dobljenih vsot.                                [20 točk]
+            /// 
+            /// V metodi ResitevNaloge že imate pripravljeni dve spremenljivki 
+            /// s potema do datotek s primeroma tabel.
+            /// V tej metodi pokličite pripravljeno metodo za obe datoteki 
+            /// in izpišite rezultata v ukazno vrstico.                                     [5 točke]
+            /// </summary>
+            /// 
+
+
+        }
+    }
+}
