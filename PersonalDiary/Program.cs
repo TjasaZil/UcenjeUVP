66c1c2c57bff7e374fd4762df8e6afb664f560cc
TjasaZil <tjasa.zilavec@gmail.com>
2025-03-26 16:21:12 +0100

    dodala

---
+++ PersonalDiary/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalDiary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        /*
+         * Aplikacija kjer lahko user vpisuje notri različne stvari in se te potem shranijo v datotelko
+         */
+    }
+}
