cc0ae9db4154f3d287ea34de71610a06fc0440ab
TjasaZil <tjasa.zilavec@gmail.com>
2025-04-01 20:18:09 +0200

    .

--- Izpit_2025_03_22/Naloga3.cs
+++ Izpit_2025_03_22/Naloga3.cs
@@ -3,22 +3,22 @@
 /// <summary>
 /// NALOGA 3:
 /// Za potrebe društva tabornikov pripravljamo aplikacijo za evidentiranje dreves v gozdovih.
-/// 
+
 /// Napišite razred Drevo, ki ima lastnosti Vrsta, Višina, Volumen in Starost. 
 /// V razredu pripravite še natanko en konstruktor, 
 /// ki kot parameter dobi vrsto drevesa. 
 /// Povozite tudi funkcijo ToString.                                            [6 točk]
-/// 
+
 /// Za razred Drevo pripravite dva podrazreda: Listavec in Iglavec. 
 /// Za vsakega od podrazredov si zamislite po eno ustrezno 
 /// specifično lastnost in jo implementirajte. 
 /// V vsakem od podrazredov ustrezno popravite še funkcijo ToString.            [7 točk]
-///     
+
 /// Napišite še razred Gozd, ki naj ima lastnosti Lokacija in Drevesa.
 /// Dodajte še funkcijo, ki vrne skupen volumen iglavcev v gozdu, 
 /// in funkcijo, ki vrne povprečno starost dreves, 
 /// katera imajo višino višjo od danega parametra.                              [7 točk]
-/// 
+ 
 /// V funkciji ResitevNaloge ustvarite eno instanco razreda Gozd 
 /// in na njegov seznam dreves dodajte štiri drevesa različnih tipov. 
 /// V ukazno vrstico s pomočjo klica ustrezne funkcije 
@@ -126,7 +126,7 @@ public class Gozd
         double skupniVolumen = 0;
         foreach (var drevo in Drevesa)
         {
-            if (drevo is Iglavec iglavec) skupniVolumen += iglavec.Volumen;
+            if (drevo is Iglavec) skupniVolumen += drevo.Volumen;
 
         }
         return skupniVolumen;
--- PrimerIzpita/Program.cs
+++ PrimerIzpita/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Naloga1.Gostje();
-            Naloga2.ResitevNaloge();
-            Naloga3.ResitevNaloge();
-            Console.Write("Kako velik naj bo metuljček?: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-            Naloga4.Metuljcek(n);
+            //Naloga1.Gostje();
+            //Naloga2.ResitevNaloge();
+            //Naloga3.ResitevNaloge();
+            //Console.Write("Kako velik naj bo metuljček?: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine();
+            //Naloga4.Metuljcek(n);
 
             Console.ReadLine();
         }
